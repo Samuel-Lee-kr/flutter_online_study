af932da7e70b925514f6845a5256d0a77d583ba9
“hopeful1213” <hopeful1213@gmail.com>
2022-04-29 16:12:24 +0900

    ..

--- problems/1_diff_problem/heechan_answer.dart
+++ problems/1_diff_problem/heechan_answer.dart
@@ -1,15 +1,30 @@
 void main() {
+  int result = 1;
   A a = A();
-  a.gggg(10);
-  print(a.result);
+  a.gggg(5);
+  print(a.result); //
+  int result2 = a.tttt(a.result);
+  print(result2); //
 }
 
 class A {
   int result = 1;
+  int fresult = 0;
   void gggg(int a) {
     result = result * a;
     if (a != 1) {
       gggg(a - 1);
     }
+  }
+
+  int tttt(int a) {
+    List<String> aa = a.toString().split('');
+    aa.forEach((element) {
+      fresult = fresult + int.parse(element);
+    });
+    if (fresult > 10) {
+      tttt(fresult);
+    }
+    return fresult;
   }
 }
