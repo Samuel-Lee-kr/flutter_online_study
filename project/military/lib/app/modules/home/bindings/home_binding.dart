424605f01a598e01f78e803087d81af13a479acd
Samuel-Lee-kr <sungyu623@naver.com>
2022-06-07 18:06:24 +0900

    [military/home/bindings] binding 수정
    1. mod: first_tab_controller 관련 binding 추가

--- project/military/lib/app/modules/home/bindings/home_binding.dart
+++ project/military/lib/app/modules/home/bindings/home_binding.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 
+import 'package:military/app/modules/home/controllers/first_tab_controller.dart';
+
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<FirstTabController>(
+      () => FirstTabController(),
+    );
     Get.lazyPut<HomeController>(
       () => HomeController(),
     );
